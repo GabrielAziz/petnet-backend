297837ba529b577ba8e0ec996210a6d6d06dca93
GuilhermeFaag <guilhermefag2@outlook.com>
2026-03-29 03:16:04 -0300

    test(cookie): ajustar sameSite para cross-site

--- src/utils/cookie.utils.js
+++ src/utils/cookie.utils.js
@@ -10,6 +10,6 @@ export const COOKIE_MAX_AGE = 24 * 60 * 60 * 1000;
 export const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     maxAge: COOKIE_MAX_AGE,
 };
